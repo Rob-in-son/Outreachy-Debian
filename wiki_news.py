97c104ae51749f6147a8c117c86b49978f6aa305
Rob-in-son <estherchidinma202@gmail.com>
2023-10-03 14:36:20 +0100

    change file from .txt to .md

--- wiki_news.py
+++ wiki_news.py
@@ -2,19 +2,24 @@
 from bs4 import BeautifulSoup
 import requests
 from markdownify import markdownify as md
+import re
 
 #website page to be scrapped
 url = "https://wiki.debian.org/News"
 # read webpage
 req = requests.get(url)
+
 #Parse data
 soup = BeautifulSoup(req.content, "html.parser")
 
 html = (soup.prettify())
+
+#convert html to markdown
 html_markdown = md(html)
 
 #Create a file to store the markdown
-markdown_file = open("Debian wiki news.txt", "x")
+markdown_file = open("Debian wiki news.md", "x")
 
 #Write to the file 
 markdown_file.write(html_markdown)
+
