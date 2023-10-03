849787099e815172a9367ef63b1389b15c7f25a8
Rob-in-son <estherchidinma202@gmail.com>
2023-10-03 13:07:25 +0100

    Create file and write to the file

--- wiki_news.py
+++ wiki_news.py
@@ -1,6 +1,7 @@
 #A script to read data from the News page on Debianwiki, parse the data and write it to a file in markdown
 from bs4 import BeautifulSoup
 import requests
+from markdownify import markdownify as md
 
 #website page to be scrapped
 url = "https://wiki.debian.org/News"
@@ -9,5 +10,11 @@ req = requests.get(url)
 #Parse data
 soup = BeautifulSoup(req.content, "html.parser")
 
-print(soup.prettify())
+html = (soup.prettify())
+html_markdown = md(html)
 
+#Create a file to store the markdown
+markdown_file = open("Debian wiki news.txt", "x")
+
+#Write to the file 
+markdown_file.write(html_markdown)
