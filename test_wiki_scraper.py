39d168eb8b7c2b32103ed6817990bd49c015b253
Robinson Uchechukwu <estherchidinma202@gmail.com>
2023-10-23 17:34:48 +0100

    Added more comments

--- test_wiki_scraper.py
+++ test_wiki_scraper.py
@@ -3,9 +3,11 @@ from unittest.mock import patch, Mock
 import requests
 from bs4 import BeautifulSoup
 from wiki_scraper import *  
+
 class TestWebScraperFunctions(unittest.TestCase):
 
     @patch('requests.get')
+    # Test fetching HTML successfully
     def test_fetch_html_success(self, mock_get):
         mock_response = Mock()
         mock_response.text = '<html></html>'
@@ -16,20 +18,24 @@ class TestWebScraperFunctions(unittest.TestCase):
 
     @patch('requests.get')
     def test_fetch_html_failure(self, mock_get):
+        # Test handling a failure in HTML fetch
         mock_get.side_effect = requests.RequestException
         result = fetch_html('http://test.com')
         self.assertIsNone(result)
-
+    
+    # Test correcting relative links in anchor tags
     def test_correct_relative_links(self):
         soup = BeautifulSoup('<a href="/test"></a>', 'html.parser')
         correct_relative_links(soup)
         self.assertEqual(soup.a['href'], 'https://wiki.debian.org/test')
 
+    # Test converting HTML to Markdown
     def test_convert_html_to_markdown(self):
         soup = BeautifulSoup('<strong>bold</strong>', 'html.parser')
         result = convert_html_to_markdown(soup)
         self.assertEqual(result, '**bold**')
 
+    # Test removing specific div elements
     def test_remove_div(self):
         soup = BeautifulSoup('<div>More Actions: test</div><div>Other text</div>', 'html.parser')
         remove_div(soup, "More Actions:")
@@ -37,6 +43,7 @@ class TestWebScraperFunctions(unittest.TestCase):
         self.assertIsNotNone(soup.find(string="Other text"))
 
     @patch('builtins.open', unittest.mock.mock_open())
+    # Test writing content to a file
     def test_write_to_file(self):
         write_to_file("test content", "test.md")
         open.assert_called_with("test.md", 'w', encoding='utf-8')
--- wiki_scraper.py
+++ wiki_scraper.py
@@ -7,10 +7,12 @@ from urllib.parse import urljoin
 BASE_URL = "https://wiki.debian.org"
 NEWS_URL = urljoin(BASE_URL, "News")
 OUTPUT_FILE = "output.md"
+
 # The div text to be excluded from the HTML
 exclude_div_text = "More Actions:"
 target_text = "Debian has several news feeds, that can be interesting fordifferent audiences, depending on how they use Debian:"
 
+#Fetch HTML content from the URL
 def fetch_html(url):
     try:
         response = requests.get(url)
@@ -20,19 +22,23 @@ def fetch_html(url):
         print(f"An error occurred: {e}")
         return None
 
+#Correct relative links to absolute URLs
 def correct_relative_links(soup):
     for a_tag in soup.find_all("a", href=True):
         if a_tag['href'].startswith('/'):
             a_tag['href'] = urljoin(BASE_URL, a_tag['href'])
 
+#Convert html to markdown
 def convert_html_to_markdown(soup):
     return md(str(soup))
 
+#Remove div element containing the specified text
 def remove_div(soup, exclude_div_text):
     for div in soup.find_all("div"):
         if exclude_div_text in div.get_text():
             div.extract()
 
+#Extract and remove a specific target line from the HTML
 def extract_target_line(soup, target_text):
     # Extract the target line
     target_line_tag = soup.find(string=target_text)
@@ -41,6 +47,7 @@ def extract_target_line(soup, target_text):
         target_line_text = target_line_tag.string + "\n\n"
         target_line_tag.extract()
 
+#Write markdown to file
 def write_to_file(content, file_name):
     try:
         with open(file_name, 'w', encoding='utf-8') as file:
@@ -49,6 +56,7 @@ def write_to_file(content, file_name):
     except IOError as e:
         print(f"An error occurred while writing to the file: {e}")
 
+#Main function
 def main():
     html_content = fetch_html(NEWS_URL)
     if html_content:
